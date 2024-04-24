315c6f2119456af8257ad73d384f42cbcede26ac
lcomp <lifesider7@gmail.com>
2024-04-24 10:07:41 +0300

    добавлен блок проверки

--- app/src/main/java/com/lifesider7/regata/MainActivity.kt
+++ app/src/main/java/com/lifesider7/regata/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lifesider7.regata
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -15,6 +19,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var signUp : TextView = findViewById(R.id.textView)
+        signUp.setOnClickListener{
+            var intent = Intent(this,Signup::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/lifesider7/regata/Signup.kt
+++ app/src/main/java/com/lifesider7/regata/Signup.kt
@@ -1,10 +1,17 @@
 package com.lifesider7.regata
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.lifesider7.regata.R.id.button
+import com.lifesider7.regata.R.id.email
+import com.lifesider7.regata.R.id.password
 
 class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,18 @@ class Signup : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var email:TextView = findViewById(email)
+        var password:TextView = findViewById(password)
+        var button:Button = findViewById(R.id.button)
+            button.setOnClickListener{
+                if(email.text.isEmpty() || email.text.contains("@"))
+                    Toast.makeText(this, "Проверьте email", Toast.LENGTH_LONG).show()
+                else if (password.text.isEmpty() || email.text.length<6)
+                    Toast.makeText(this, "Пароль должен быть больше шести символов", Toast.LENGTH_LONG).show()
+                else{
+                    //code will be here
+                }
+                }
+
     }
 }
